712527d089858964faf507090f670689a19f676c
Dion Weiman <dion.18@itfac.mrt.ac.lk>
2020-11-15 02:03:27 +0530

    Change log
    This is the actual backend code

---
+++ api/Routes/user.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const dbService = require("../controller/userDbService");
+const { route } = require("../../app");
+const db = dbService.getDbServiceInstance();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+router.get("/getUsers", (req, res) => {
+  const result = db.getAllData();
+
+  result
+    .then((data) => {
+      console.log(data);
+
+      res.json({ data: data });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.post("/signup", (req, res) => {
+  const checkUser = db.checkExistingUser(req.body.email);
+
+  checkUser
+    .then((user) => {
+      if (!user) {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const userID = req.body.userID;
+            const name = req.body.name;
+            const email = req.body.email;
+            const password = hash;
+            const designation = req.body.designation;
+            const roleID = req.body.roleID;
+            const result = db.createNewUser(
+              userID,
+              name,
+              email,
+              password,
+              designation,
+              roleID
+            );
+
+            result
+              .then((reply) => res.json(reply))
+              .catch((err) => res.json(err));
+          }
+        });
+      } else {
+        console.log("This user already exists");
+        res.status(409).json({
+          message: "This user already exits",
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+router.post("/login", (req, res) => {
+  const checkUser = db.checkExistingUser(req.body.email);
+
+  checkUser
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed, User doesn't exist",
+        });
+      } else {
+        userPassword = user.Password;
+        console.log(userPassword);
+        console.log(req.body.password);
+        bcrypt.compare(req.body.password, userPassword, (err, result) => {
+          if (err) {
+            return res.status(401).json({
+              message: "Auth failed, there was an error verifying the password",
+            });
+          }
+          if (result) {
+            const token = jwt.sign(
+              {
+                email: user.Email,
+                userId: user.userID,
+              },
+              process.env.JWT_KEY,
+              {
+                expiresIn: "1h",
+              }
+            );
+            return res.status(200).json({
+              message: "Auth Successful",
+              token: token,
+            });
+          }
+          res.status(401).json({
+            message: "Auth failed, Password is incorrect",
+          });
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+});
+
+router.delete("/:userId", (req, res) => {
+  const result = db.deleteUser(req.params.userId);
+
+  console.log(req.params.userId);
+
+  result
+    .then((data) => {
+      console.log(data);
+
+      res.json({ data: data });
+    })
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
---
+++ api/controller/userDbService.js
@@ -0,0 +1,81 @@
+const mysql = require("mysql");
+let instance = null;
+
+const connection = require("./dbConnection");
+
+class Dbservice {
+  static getDbServiceInstance() {
+    return instance ? instance : new Dbservice();
+  }
+
+  async getAllData() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM user";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async createNewUser(userID, name, email, password, designation, roleID) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "INSERT INTO user VALUES (?, ?, ?, ?, ?, ?) ";
+
+        connection.query(
+          query,
+          [userID, name, email, password, designation, roleID],
+          (err, result) => {
+            if (err) reject(new Error(err));
+            resolve({ Status: "Sucessful" });
+          }
+        );
+      });
+
+      return response;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  async checkExistingUser(email) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM user WHERE email=?";
+
+        connection.query(query, [email], (err, results) => {
+          if (err) reject(new Error(err));
+          resolve(results[0]);
+        });
+      });
+      return response;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  async deleteUser(id) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "DELETE from user WHERE UserID=?";
+
+        connection.query(query, [id], (err, results) => {
+          if (err) reject(new Error(err));
+          resolve({ message: "This record was removed" });
+        });
+      });
+      return response;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+}
+
+module.exports = Dbservice;
