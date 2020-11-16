05669713992ddb7ecec908586e69e7af31dc6ce4
yomalp <yomalp777@gmail.com>
2020-11-16 21:22:25 +0530

    Added the getUnassignedRepairs API endpoint to receive repairs without an engineer assigned

--- api/Routes/repair.js
+++ api/Routes/repair.js
@@ -1,56 +1,62 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
-const dbService = require("../controller/repairDbService");
+const dbService = require('../controller/repairDbService');
 const db = dbService.getDbServiceInstance();
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.get("/getRepairs", (req, res) => {
-  const result = db.getAllRepairs();
+router.get('/getRepairs', (req, res) => {
+	const result = db.getAllRepairs();
 
-  result
-    .then((data) => {
-      console.log(data);
-      res.json({ data: data });
-    })
-    .catch((err) => console.log(err));
+	result
+		.then((data) => {
+			console.log(data);
+			res.json({ data: data });
+		})
+		.catch((err) => console.log(err));
 });
 
-router.patch("/addCompletedDateAndComments", (req, res) => {
-  let assetId = req.body.AssetID;
-  let createdDate = req.body.CreatedDate;
-  let completedDate = req.body.CompletedDate;
-  let comments = req.body.comments;
-  console.log(req.body);
+router.get('/getUnassignedRepairs', (req, res) => {
+	const result = db.getUnassignedRepairs();
 
-  const result = db.addCompletedDateAndComments(
-    assetId,
-    createdDate,
-    completedDate,
-    comments
-  );
-
-  result
-    .then((reply) => {
-      res.json({
-        message: "Date changed",
-      });
-    })
-    .catch((err) => console.log(err));
+	result
+		.then((data) => {
+			console.log(data);
+			res.json({ data: data });
+		})
+		.catch((err) => console.log(err));
 });
 
-router.post("/addRepair", (req, res) => {
-  let assetId = req.body.AssetId;
-  let createdDate = req.body.CreatedDate;
+router.patch('/addCompletedDateAndComments', (req, res) => {
+	let assetId = req.body.AssetID;
+	let createdDate = req.body.CreatedDate;
+	let completedDate = req.body.CompletedDate;
+	let comments = req.body.comments;
+	console.log(req.body);
 
-  console.log(req.body);
+	const result = db.addCompletedDateAndComments(assetId, createdDate, completedDate, comments);
 
-  const result = db.addRepair(assetId, createdDate);
+	result
+		.then((reply) => {
+			res.json({
+				message: 'Date changed'
+			});
+		})
+		.catch((err) => console.log(err));
+});
 
-  result.then((reply) => res.json(reply)).catch((err) => console.log(err));
+router.post('/addRepair', (req, res) => {
+	let assetId = req.body.AssetId;
+	let createdDate = req.body.CreatedDate;
+
+	console.log(req.body);
+
+	const result = db.addRepair(assetId, createdDate);
+
+	result.then((reply) => res.json(reply)).catch((err) => console.log(err));
 });
 
 module.exports = router;
--- api/controller/repairDbService.js
+++ api/controller/repairDbService.js
@@ -1,90 +1,90 @@
-const mysql = require("mysql");
-const connection = require("./dbConnection");
+const mysql = require('mysql');
+const connection = require('./dbConnection');
 let instance = null;
 
 class Dbservice {
-  static getDbServiceInstance() {
-    return instance ? instance : new Dbservice();
-  }
+	static getDbServiceInstance() {
+		return instance ? instance : new Dbservice();
+	}
 
-  async getAllRepairs() {
-    try {
-      const response = await new Promise((resolve, reject) => {
-        const query = "SELECT * FROM repair";
+	async getAllRepairs() {
+		try {
+			const response = await new Promise((resolve, reject) => {
+				const query = 'SELECT * FROM repair';
 
-        connection.query(query, (err, results) => {
-          if (err) reject(new Error(err));
-          resolve(results);
-        });
-      });
-      return response;
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+				connection.query(query, (err, results) => {
+					if (err) reject(new Error(err));
+					resolve(results);
+				});
+			});
+			return response;
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
+	async getUnassignedRepairs() {
+		try {
+			const response = await new Promise((resolve, reject) => {
+				const query = 'SELECT * FROM repair WHERE EngineerID IS NULL';
 
-  async addRepair(assetID, createdDate) {
-    try {
-      const response = await new Promise((resolve, reject) => {
-        const query = "INSERT INTO repair(AssetID, CreatedDate) VALUES (?,?)";
+				connection.query(query, (err, results) => {
+					if (err) reject(new Error(err));
+					resolve(results);
+				});
+			});
+			return response;
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
 
-        connection.query(query, [assetID, createdDate], (err, results) => {
-          if (err) reject(err.message);
-          resolve("Record added");
-        });
-      });
-      return response;
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+	async addRepair(assetID, createdDate) {
+		try {
+			const response = await new Promise((resolve, reject) => {
+				const query = 'INSERT INTO repair(AssetID, CreatedDate) VALUES (?,?)';
 
-  async assignEngineer(assetID, createdDate, engineerID) {
-    try {
-      const response = await new Promise((resolve, reject) => {
-        const query =
-          "UPDATE repair SET EngineerID = ? WHERE CreatedDate = ? AND AssetID = ?";
+				connection.query(query, [ assetID, createdDate ], (err, results) => {
+					if (err) reject(err.message);
+					resolve('Record added');
+				});
+			});
+			return response;
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
 
-        connection.query(
-          query,
-          [engineerID, createdDate, assetID],
-          (err, results) => {
-            if (err) reject(err.message);
-            resolve("Engineer assigned");
-          }
-        );
-      });
-      return response;
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+	async assignEngineer(assetID, createdDate, engineerID) {
+		try {
+			const response = await new Promise((resolve, reject) => {
+				const query = 'UPDATE repair SET EngineerID = ? WHERE CreatedDate = ? AND AssetID = ?';
 
-  async addCompletedDateAndComments(
-    assetID,
-    createdDate,
-    completedDate,
-    comments
-  ) {
-    try {
-      const response = await new Promise((resolve, reject) => {
-        const query =
-          "UPDATE repair SET CompletedDate = ?, comments = ? WHERE AssetID = ? AND CreatedDate = ?";
+				connection.query(query, [ engineerID, createdDate, assetID ], (err, results) => {
+					if (err) reject(err.message);
+					resolve('Engineer assigned');
+				});
+			});
+			return response;
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
 
-        connection.query(
-          query,
-          [completedDate, comments, assetID, createdDate],
-          (err, results) => {
-            if (err) reject(err.message);
-            resolve("CompletedDate changed");
-          }
-        );
-      });
-      return response;
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+	async addCompletedDateAndComments(assetID, createdDate, completedDate, comments) {
+		try {
+			const response = await new Promise((resolve, reject) => {
+				const query = 'UPDATE repair SET CompletedDate = ?, comments = ? WHERE AssetID = ? AND CreatedDate = ?';
+
+				connection.query(query, [ completedDate, comments, assetID, createdDate ], (err, results) => {
+					if (err) reject(err.message);
+					resolve('CompletedDate changed');
+				});
+			});
+			return response;
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
 }
 
 module.exports = Dbservice;
