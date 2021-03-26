3bcc62190e224e1079dcd78b594c92b586bceeab
rukshankr <rukshankr@users.noreply.github.com>
2021-03-26 06:19:12 +0530

    test values as json

--- api/controller/testDbService.js
+++ api/controller/testDbService.js
@@ -143,7 +143,7 @@ class Dbservice {
 				CONCAT("'",comments,"']")
 				) 
 				,"]") 
-				AS values FROM test`;
+				AS json FROM test`;
 
 				connection.query(query, (err, results) => {
 					if (err) reject(new Error(err));
