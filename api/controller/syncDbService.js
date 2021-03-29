81b69469dc4f0b70646c5d9b02a15d8fbbc9b05d
rukshankr <rukshankr@users.noreply.github.com>
2021-03-29 08:08:37 +0530

    sync table change

--- api/controller/syncDbService.js
+++ api/controller/syncDbService.js
@@ -14,9 +14,47 @@ class Dbservice {
 		try {
 			const response = await new Promise((resolve, reject) => {
 				const query = `START TRANSACTION;
-                SELECT * AS test FROM test where DateCompleted is NULL;
-                SELECT * AS asset FROM asset;
-                SELECT * AS repair FROM repair;
+                SELECT CONCAT("{", 
+				GROUP_CONCAT( 
+				CONCAT("['",TestID,"',"),
+				CONCAT("'",DateIssued,"',"),
+				CONCAT("'",AssetID,"',"),
+				CONCAT("'",InspectorID,"',"),
+				CONCAT("'",Result,"',"),
+				CONCAT("'",SupervisorID,"',"),
+				CONCAT("'",DateCompleted,"',"),
+				CONCAT("'",Frequency,"',"),
+				CONCAT("'",Priority,"',"),
+				CONCAT("'",TestModID,"',"),
+				CONCAT("'",comments,"']")
+				) 
+				,"}") 
+				as json FROM test where DateCompleted is NULL;
+                SELECT CONCAT("{", 
+				GROUP_CONCAT( 
+				CONCAT("['",AssetID,"',"),
+				CONCAT("'",AssetType,"',"),
+				CONCAT("'",Status,"',"),
+				CONCAT("'",GPSLatitude,"',"),
+				CONCAT("'",GPSLongitude,"',"),
+				CONCAT("'",Region,"',"),
+				CONCAT("'",Division,"',"),
+				CONCAT("'",SubDivision,"',"),
+                CONCAT("'",NearestMilePost,"',"),
+				CONCAT("'",LastTestedDate,"']")
+				) 
+				,"}") 
+				as arson FROM asset;
+                SELECT CONCAT("{", 
+				GROUP_CONCAT( 
+				CONCAT("['",EngineerID,"',"),
+				CONCAT("'",AssetID,"',"),
+				CONCAT("'",CreatedDate,"',"),
+				CONCAT("'",CompletedDate,"',"),
+				CONCAT("'",comments,"']")
+				) 
+				,"}") 
+				as reparson FROM repair;
                 COMMIT;
                 `;
 
