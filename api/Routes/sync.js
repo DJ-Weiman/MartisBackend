def3c1c248baee0fa545a500ec6e843c8f770f52
rukshankr <rukshankr@users.noreply.github.com>
2021-03-29 17:27:15 +0530

    bye

--- api/Routes/sync.js
+++ api/Routes/sync.js
@@ -15,33 +15,61 @@ router.get('/export', (req, res) => {
 	result
 		.then((data) => {
 			console.log(data);
-			const dataToImport = {
-				database : "martis",
-				version : 1,
-				encrypted : false,
-				mode : "full",
-				tables :[
-					{
-						name: "asset",
-						schema: [
-							{column:"AssetID", value: "TEXT PRIMARY KEY NOT NULL"},
-							{column:"AssetType", value:"TEXT DEFAULT NULL"},
-							{column:"Status", value:"TEXT DEFAULT NULL"},
-							{column:"GPSLatitude", value:"TEXT DEFAULT NULL"},
-							{column:"GPSLongitude", value:"TEXT DEFAULT NULL"},
-							{column:"Region", value:"TEXT DEFAULT NULL"},
-							{column:"Division", value:"TEXT DEFAULT NULL"},
-							{column:"SubDivision", value:"TEXT DEFAULT NULL"},
-							{column:"NearestMilePost", value:"TEXT DEFAULT NULL"},
-							{column:"LastTestedDate", value:"TEXT DEFAULT NULL"}
-						],
-						values: data
-					}
-				]
-			};
-			res.json(dataToImport);
+			var assetData = data;
+		//	res.json(dataToImport);
 		})
 		.catch((err) => console.log(err));
+	
+	const testResults = db.exportTests();
+
+	testResults
+		.then((data) => {
+			var testData = data;
+		})
+		.catch((err) => console.log(err));
+
+		const dataToImport = {
+			database : "martis",
+			version : 1,
+			encrypted : false,
+			mode : "full",
+			tables :[
+				{
+					name: "asset",
+					schema: [
+						{column:"AssetID", value: "TEXT PRIMARY KEY NOT NULL"},
+						{column:"AssetType", value:"TEXT DEFAULT NULL"},
+						{column:"Status", value:"TEXT DEFAULT NULL"},
+						{column:"GPSLatitude", value:"TEXT DEFAULT NULL"},
+						{column:"GPSLongitude", value:"TEXT DEFAULT NULL"},
+						{column:"Region", value:"TEXT DEFAULT NULL"},
+						{column:"Division", value:"TEXT DEFAULT NULL"},
+						{column:"SubDivision", value:"TEXT DEFAULT NULL"},
+						{column:"NearestMilePost", value:"TEXT DEFAULT NULL"},
+						{column:"LastTestedDate", value:"TEXT DEFAULT NULL"}
+					],
+					values: assetData
+				},
+				{
+					name: "test",
+					schema: [
+						{column:"TestID", value: "TEXT PRIMARY KEY NOT NULL"},
+						{column:"AssetType", value:"TEXT DEFAULT NULL"},
+						{column:"Status", value:"TEXT DEFAULT NULL"},
+						{column:"GPSLatitude", value:"TEXT DEFAULT NULL"},
+						{column:"GPSLongitude", value:"TEXT DEFAULT NULL"},
+						{column:"Region", value:"TEXT DEFAULT NULL"},
+						{column:"Division", value:"TEXT DEFAULT NULL"},
+						{column:"SubDivision", value:"TEXT DEFAULT NULL"},
+						{column:"NearestMilePost", value:"TEXT DEFAULT NULL"},
+						{column:"LastTestedDate", value:"TEXT DEFAULT NULL"}
+					],
+					values: testData
+				}
+			]
+		};
+
+		res.json(dataToImport);
 });
 
 
