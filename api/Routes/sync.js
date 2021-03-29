4301dc0123e67d400da687c037d8b19315c0c709
rukshankr <rukshankr@users.noreply.github.com>
2021-03-29 17:31:10 +0530

    andes

--- api/Routes/sync.js
+++ api/Routes/sync.js
@@ -16,60 +16,57 @@ router.get('/export', (req, res) => {
 		.then((data) => {
 			console.log(data);
 			var assetData = data;
+			const testResults = db.exportTests();
+			testResults
+			.then((data) => {
+				var testData = data;
+				const dataToImport = {
+					database : "martis",
+					version : 1,
+					encrypted : false,
+					mode : "full",
+					tables :[
+						{
+							name: "asset",
+							schema: [
+								{column:"AssetID", value: "TEXT PRIMARY KEY NOT NULL"},
+								{column:"AssetType", value:"TEXT DEFAULT NULL"},
+								{column:"Status", value:"TEXT DEFAULT NULL"},
+								{column:"GPSLatitude", value:"TEXT DEFAULT NULL"},
+								{column:"GPSLongitude", value:"TEXT DEFAULT NULL"},
+								{column:"Region", value:"TEXT DEFAULT NULL"},
+								{column:"Division", value:"TEXT DEFAULT NULL"},
+								{column:"SubDivision", value:"TEXT DEFAULT NULL"},
+								{column:"NearestMilePost", value:"TEXT DEFAULT NULL"},
+								{column:"LastTestedDate", value:"TEXT DEFAULT NULL"}
+							],
+							values: assetData
+						},
+						{
+							name: "test",
+							schema: [
+								{column:"TestID", value: "TEXT PRIMARY KEY NOT NULL"},
+								{column:"AssetType", value:"TEXT DEFAULT NULL"},
+								{column:"Status", value:"TEXT DEFAULT NULL"},
+								{column:"GPSLatitude", value:"TEXT DEFAULT NULL"},
+								{column:"GPSLongitude", value:"TEXT DEFAULT NULL"},
+								{column:"Region", value:"TEXT DEFAULT NULL"},
+								{column:"Division", value:"TEXT DEFAULT NULL"},
+								{column:"SubDivision", value:"TEXT DEFAULT NULL"},
+								{column:"NearestMilePost", value:"TEXT DEFAULT NULL"},
+								{column:"LastTestedDate", value:"TEXT DEFAULT NULL"}
+							],
+							values: testData
+						}
+					]
+				};
+				res.json(dataToImport);
+			})
+			.catch((err) => console.log(err));
+
 		//	res.json(dataToImport);
 		})
 		.catch((err) => console.log(err));
-	
-	const testResults = db.exportTests();
-
-	testResults
-		.then((data) => {
-			var testData = data;
-		})
-		.catch((err) => console.log(err));
-
-		const dataToImport = {
-			database : "martis",
-			version : 1,
-			encrypted : false,
-			mode : "full",
-			tables :[
-				{
-					name: "asset",
-					schema: [
-						{column:"AssetID", value: "TEXT PRIMARY KEY NOT NULL"},
-						{column:"AssetType", value:"TEXT DEFAULT NULL"},
-						{column:"Status", value:"TEXT DEFAULT NULL"},
-						{column:"GPSLatitude", value:"TEXT DEFAULT NULL"},
-						{column:"GPSLongitude", value:"TEXT DEFAULT NULL"},
-						{column:"Region", value:"TEXT DEFAULT NULL"},
-						{column:"Division", value:"TEXT DEFAULT NULL"},
-						{column:"SubDivision", value:"TEXT DEFAULT NULL"},
-						{column:"NearestMilePost", value:"TEXT DEFAULT NULL"},
-						{column:"LastTestedDate", value:"TEXT DEFAULT NULL"}
-					],
-					values: assetData
-				},
-				{
-					name: "test",
-					schema: [
-						{column:"TestID", value: "TEXT PRIMARY KEY NOT NULL"},
-						{column:"AssetType", value:"TEXT DEFAULT NULL"},
-						{column:"Status", value:"TEXT DEFAULT NULL"},
-						{column:"GPSLatitude", value:"TEXT DEFAULT NULL"},
-						{column:"GPSLongitude", value:"TEXT DEFAULT NULL"},
-						{column:"Region", value:"TEXT DEFAULT NULL"},
-						{column:"Division", value:"TEXT DEFAULT NULL"},
-						{column:"SubDivision", value:"TEXT DEFAULT NULL"},
-						{column:"NearestMilePost", value:"TEXT DEFAULT NULL"},
-						{column:"LastTestedDate", value:"TEXT DEFAULT NULL"}
-					],
-					values: testData
-				}
-			]
-		};
-
-		res.json(dataToImport);
 });
 
 
