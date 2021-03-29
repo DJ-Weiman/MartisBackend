a219a9d36745858852d8c8b2efd2d95a91f7f909
rukshankr <rukshankr@users.noreply.github.com>
2021-03-29 17:17:28 +0530

    bep

--- api/Routes/sync.js
+++ api/Routes/sync.js
@@ -15,9 +15,35 @@ router.get('/export', (req, res) => {
 	result
 		.then((data) => {
 			console.log(data);
-			res.json({ values: data });
+			const dataToImport = {
+				database : "martis",
+				version : 1,
+				encrypted : false,
+				mode : "full",
+				tables :[
+					{
+						name: "asset",
+						schema: [
+							{column:"AssetID", value: "TEXT PRIMARY KEY NOT NULL"},
+							{column:"AssetType", value:"TEXT DEFAULT NULL"},
+							{column:"Status", value:"TEXT DEFAULT NULL"},
+							{column:"GPSLatitude", value:"TEXT DEFAULT NULL"},
+							{column:"GPSLongitude", value:"TEXT DEFAULT NULL"},
+							{column:"Region", value:"TEXT DEFAULT NULL"},
+							{column:"Division", value:"TEXT DEFAULT NULL"},
+							{column:"SubDivision", value:"TEXT DEFAULT NULL"},
+							{column:"NearestMilePost", value:"TEXT DEFAULT NULL"},
+							{column:"LastTestedDate", value:"TEXT DEFAULT NULL"}
+						],
+						values: data
+					}
+				]
+			};
+			res.json(dataToImport);
 		})
 		.catch((err) => console.log(err));
 });
+
+
 
 module.exports = router;
