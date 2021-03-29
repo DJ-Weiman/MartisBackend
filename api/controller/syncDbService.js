b8a015323ed2bfffd6b84f4e546569cb5fc6ecf0
rukshankr <rukshankr@users.noreply.github.com>
2021-03-29 11:24:40 +0530

    sync thing back

--- api/controller/syncDbService.js
+++ api/controller/syncDbService.js
@@ -14,47 +14,32 @@ class Dbservice {
 		try {
 			const response = await new Promise((resolve, reject) => {
 				const query = `START TRANSACTION;
-                SELECT CONCAT("{", 
-				GROUP_CONCAT( 
-				CONCAT("['",TestID,"',"),
-				CONCAT("'",DateIssued,"',"),
-				CONCAT("'",AssetID,"',"),
-				CONCAT("'",InspectorID,"',"),
-				CONCAT("'",Result,"',"),
-				CONCAT("'",SupervisorID,"',"),
-				CONCAT("'",DateCompleted,"',"),
-				CONCAT("'",Frequency,"',"),
-				CONCAT("'",Priority,"',"),
-				CONCAT("'",TestModID,"',"),
-				CONCAT("'",comments,"']")
-				) 
-				,"}") 
-				as json FROM test where DateCompleted is NULL;
-                SELECT CONCAT("{", 
-				GROUP_CONCAT( 
-				CONCAT("['",AssetID,"',"),
-				CONCAT("'",AssetType,"',"),
-				CONCAT("'",Status,"',"),
-				CONCAT("'",GPSLatitude,"',"),
-				CONCAT("'",GPSLongitude,"',"),
-				CONCAT("'",Region,"',"),
-				CONCAT("'",Division,"',"),
-				CONCAT("'",SubDivision,"',"),
-                CONCAT("'",NearestMilePost,"',"),
-				CONCAT("'",LastTestedDate,"']")
-				) 
-				,"}") 
-				as arson FROM asset;
-                SELECT CONCAT("{", 
-				GROUP_CONCAT( 
-				CONCAT("['",EngineerID,"',"),
-				CONCAT("'",AssetID,"',"),
-				CONCAT("'",CreatedDate,"',"),
-				CONCAT("'",CompletedDate,"',"),
-				CONCAT("'",comments,"']")
-				) 
-				,"}") 
-				as reparson FROM repair;
+                SELECT * FROM asset;
+                SELECT * FROM repair;
+                SELECT * FROM test WHERE CompletedDate IS NULL;
+                COMMIT;
+                `;
+
+				connection.query(query, (err, results) => {
+					if (err) reject(new Error(err));
+					resolve(results);
+				});
+			});
+			return response;
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
+
+    async exAll() {
+		try {
+            i = 0;
+            for (i = 0; i < )
+			const response = await new Promise((resolve, reject) => {
+				const query = `START TRANSACTION;
+                SELECT * FROM asset;
+                SELECT * FROM repair;
+                SELECT * FROM test WHERE CompletedDate IS NULL;
                 COMMIT;
                 `;
 
