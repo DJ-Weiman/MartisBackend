53934e8fb80b86b05815fa7b5c781ac5e9d82243
rukshankr <rukshankr@users.noreply.github.com>
2021-03-31 19:31:16 +0530

    partialexport

--- api/Routes/sync.js
+++ api/Routes/sync.js
@@ -410,6 +410,117 @@ router.get("/export", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+router.get("/partialexport", (req, res) => {
+  const result = db.exportAssets();
+
+  result
+    .then((data) => {
+      console.log(data);
+      var assetData = data;
+      const testResults = db.exportTests();
+      testResults
+        .then((data2) => {
+          var testData = data2;
+          const repairResults = db.exportRepairs();
+
+          repairResults
+            .then((data3) => {
+              var repairData = data3;
+              const accessResults = db.exportAccess();
+
+              accessResults
+                .then((data4) => {
+                  var accessData = data4;
+                  const roleResults = db.exportRoles();
+
+                  roleResults
+                    .then((data5) => {
+                      var roleData = data5;
+                      const userResults = db.exportUsers();
+
+                      userResults
+                        .then((data6) => {
+                          var userData = data6;
+                          const deviceResults = db.exportDevices();
+
+                          deviceResults
+                            .then((data7) => {
+                              var deviceData = data7;
+                              const roleaccessResults = db.exportRoleAccess();
+
+                              roleaccessResults
+                                .then((data8) => {
+                                  var roleaccessData = data8;
+                                  const testmoduleResults = db.exportTestModules();
+
+                                  testmoduleResults
+                                    .then((data9) => {
+                                      var testmoduleData = data9;
+                                      const dataToImport = {
+                                        database: "martis",
+                                        version: 1,
+                                        encrypted: false,
+                                        mode: "partial",
+                                        tables: [
+                                          {
+                                            name: "access",
+                                            values: accessData,
+                                          },
+                                          {
+                                            name: "asset",
+                                            values: assetData,
+                                          },
+                                          {
+                                            name: "role",
+                                            values: roleData,
+                                          },
+                                          {
+                                            name: "user",
+                                            values: userData,
+                                          },
+                                          {
+                                            name: "device",
+                                            values: deviceData,
+                                          },
+                                          {
+                                            name: "repair",
+                                            values: repairData,
+                                          },
+                                          {
+                                            name: "roleaccess",
+                                            values: roleaccessData,
+                                          },
+                                          {
+                                            name: "testmodule",
+                                            values: testmoduleData,
+                                          },
+                                          {
+                                            name: "test",
+                                            values: testData,
+                                          },
+                                        ],
+                                      };
+                                      res.json(dataToImport);
+                                    })
+                                    .catch((err) => console.log(err));
+                                })
+                                .catch((err) => console.log(err));
+                            })
+                            .catch((err) => console.log(err));
+                        })
+                        .catch((err) => console.log(err));
+                    })
+                    .catch((err) => console.log(err));
+                })
+                .catch((err) => console.log(err));
+            })
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+});
+
 router.get("/exportTests", (req, res) => {
   const result = db.exportTests();
 
