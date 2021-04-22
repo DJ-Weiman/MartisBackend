b84a463607a08ca95511cdfcd74ea33ba1769665
Dion Weiman <dion.18@itfac.mrt.ac.lk>
2021-04-22 14:05:04 +0530

    Added API endpoints for the device table

---
+++ api/Routes/device.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+
+const dbService = require("../controller/deviceDbService");
+const { urlencoded } = require("body-parser");
+const db = dbService.getDbServiceInstance();
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+router.get("/getdevices", (req, res) => {
+  const result = db.getAllDevices();
+
+  result
+    .then((data) => {
+      console.log(data);
+      res.json({ data: data });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.post("/getDeviceByID", (req, res) => {
+  let deviceId = req.body.deviceId;
+
+  console.log(req.body);
+
+  const result = db.getDeviceByID(deviceId);
+
+  result
+    .then((data) => {
+      //console.log(data);
+      res.json({ data: data });
+    })
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
---
+++ api/controller/deviceDbService.js
@@ -0,0 +1,45 @@
+const mysql = require("mysql");
+let instance = null;
+
+const connection = require("./dbConnection");
+
+class Dbservice {
+  static getDbServiceInstance() {
+    return instance ? instance : new Dbservice();
+  }
+
+  async getAllDevices() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM device";
+
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async getDeviceByID(deviceID) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM device d WHERE d.DeviceID=? ";
+
+        connection.query(query, [deviceID], (err, result) => {
+          if (err) reject(new Error(err));
+          resolve(result);
+        });
+      });
+
+      return response;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+}
+
+module.exports = Dbservice;
