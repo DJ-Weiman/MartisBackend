919aaa65c7c5ddff25f62471153ffe981b1065db
Dion Weiman <dion.18@itfac.mrt.ac.lk>
2021-05-24 20:17:22 +0530

    Added Inspection list sorting by EMPID

--- api/Routes/test.js
+++ api/Routes/test.js
@@ -1,145 +1,176 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-const dbService = require('../controller/testDbService');
-const { urlencoded } = require('body-parser');
+const bodyParser = require("body-parser");
+const dbService = require("../controller/testDbService");
+const { urlencoded } = require("body-parser");
 const db = dbService.getDbServiceInstance();
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.get('/getTests', (req, res) => {
-	const result = db.getAllTests();
-	result
-		.then((data) => {
-			console.log(data);
-			res.json({ data: data });
-		})
-		.catch((err) => console.log(err));
+router.get("/getTests", (req, res) => {
+  const result = db.getAllTests();
+  result
+    .then((data) => {
+      console.log(data);
+      res.json({ data: data });
+    })
+    .catch((err) => console.log(err));
 });
 
-router.get('/exportTests', (req, res) => {
-	const result = db.exportTests();
-	result
-		.then((data) => {
-			console.log(data);
-			res.json(data);
-		})
-		.catch((err) => console.log(err));
+router.get("/exportTests", (req, res) => {
+  const result = db.exportTests();
+  result
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((err) => console.log(err));
 });
 
-router.post('/importTest', (req, res) => {
-	let TestID = req.body.TestID;
-	let DateIssued = req.body.DateIssued;
-	let AssetID = req.body.AssetID;
-	let InspectorID = req.body.InspectorID;
-	let Result = req.body.Result;
-	let SupervisorID = req.body.SupervisorID;
-	let DateCompleted = req.body.DateCompleted;
-	let Frequency = req.body.Frequency;
-	let Priority = req.body.Priority;
-	let TestModID = req.body.TestModID;
-	let comments = req.body.comments;
-	console.log(req.body);
-	const result = db.importTest(
-		TestID,
-		DateIssued,
-		AssetID,
-		InspectorID,
-		Result,
-		SupervisorID,
-		DateCompleted,
-		Frequency,
-		Priority,
-		TestModID,
-		comments
-	);
-	result
-		.then((reply) => {
-			res.json({
-				message: 'Test imported successfully',
-				reply: reply
-			});
-		})
-		.catch((err) => console.log(err));
+router.post("/importTest", (req, res) => {
+  let TestID = req.body.TestID;
+  let DateIssued = req.body.DateIssued;
+  let AssetID = req.body.AssetID;
+  let InspectorID = req.body.InspectorID;
+  let Result = req.body.Result;
+  let SupervisorID = req.body.SupervisorID;
+  let DateCompleted = req.body.DateCompleted;
+  let Frequency = req.body.Frequency;
+  let Priority = req.body.Priority;
+  let TestModID = req.body.TestModID;
+  let comments = req.body.comments;
+  console.log(req.body);
+  const result = db.importTest(
+    TestID,
+    DateIssued,
+    AssetID,
+    InspectorID,
+    Result,
+    SupervisorID,
+    DateCompleted,
+    Frequency,
+    Priority,
+    TestModID,
+    comments
+  );
+  result
+    .then((reply) => {
+      res.json({
+        message: "Test imported successfully",
+        reply: reply,
+      });
+    })
+    .catch((err) => console.log(err));
 });
 
-router.post('/createNewTest', (req, res) => {
-	let TestID = req.body.TestID;
-	let DateIssued = req.body.DateIssued;
-	let AssetID = req.body.AssetID;
-	let InspectorID = req.body.InspectorID;
-	let SupervisorID = req.body.SupervisorID;
-	let Frequency = req.body.Frequency;
-	let TestModID = req.body.TestModID;
-	let Priority = req.body.Priority;
-	console.log(req.body);
-	const result = db.createNewTest(
-		TestID,
-		DateIssued,
-		AssetID,
-		InspectorID,
-		SupervisorID,
-		Frequency,
-		Priority,
-		TestModID
-	);
-	result
-		.then((reply) => {
-			res.json({
-				message: 'Test added successfully',
-				reply: reply
-			});
-		})
-		.catch((err) => console.log(err));
+router.post("/createNewTest", (req, res) => {
+  let TestID = req.body.TestID;
+  let DateIssued = req.body.DateIssued;
+  let AssetID = req.body.AssetID;
+  let InspectorID = req.body.InspectorID;
+  let SupervisorID = req.body.SupervisorID;
+  let Frequency = req.body.Frequency;
+  let TestModID = req.body.TestModID;
+  let Priority = req.body.Priority;
+  console.log(req.body);
+  const result = db.createNewTest(
+    TestID,
+    DateIssued,
+    AssetID,
+    InspectorID,
+    SupervisorID,
+    Frequency,
+    Priority,
+    TestModID
+  );
+  result
+    .then((reply) => {
+      res.json({
+        message: "Test added successfully",
+        reply: reply,
+      });
+    })
+    .catch((err) => console.log(err));
 });
 
-router.patch('/setResult', (req, res) => {
-	let AssetID = req.body.AssetID;
-	let TestID = req.body.TestID;
-	let Result = req.body.Result;
-	let DateCompleted = req.body.DateCompleted;
-	let comments = req.body.comments;
-	console.log(req.body);
-	const result = db.setResult(TestID, Result, DateCompleted, comments);
+router.patch("/setResult", (req, res) => {
+  let AssetID = req.body.AssetID;
+  let TestID = req.body.TestID;
+  let Result = req.body.Result;
+  let DateCompleted = req.body.DateCompleted;
+  let comments = req.body.comments;
+  console.log(req.body);
+  const result = db.setResult(TestID, Result, DateCompleted, comments);
 
-	result
-		.then((reply) => {
-			res.json({
-				message: reply
-			});
-		})
-		.catch((err) => console.log(err));
-	if (Result == 'Fail') {
-		const trigger = db.AutoInsertIntoRepair(AssetID, DateCompleted);
-		trigger
-			.then((reply) => {
-				console.log('The repair record was added');
-			})
-			.catch((err) => console.log(err));
-	}
+  result
+    .then((reply) => {
+      res.json({
+        message: reply,
+      });
+    })
+    .catch((err) => console.log(err));
+  if (Result == "Fail") {
+    const trigger = db.AutoInsertIntoRepair(AssetID, DateCompleted);
+    trigger
+      .then((reply) => {
+        console.log("The repair record was added");
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
-router.post('/orderByLocationAndInspector', (req, res) => {
-	const empLatitude = req.body.empLatitude;
-	const empLongitude = req.body.empLongitude;
-	const empId = req.body.empId;
-	const result = db.orderByLocationAndInspector(empId, empLatitude, empLongitude);
-	result
-		.then((data) => {
-			res.json({ data: data });
-		})
-		.catch((err) => console.log(err));
+router.post("/orderByLocationAndInspector", (req, res) => {
+  const empLatitude = req.body.empLatitude;
+  const empLongitude = req.body.empLongitude;
+  const empId = req.body.empId;
+  const result = db.orderByLocationAndInspector(
+    empId,
+    empLatitude,
+    empLongitude
+  );
+  result
+    .then((data) => {
+      res.json({ data: data });
+    })
+    .catch((err) => console.log(err));
 });
 
-router.get('/orderByPriority', (req, res) => {
-	const result = db.orderByPriority();
-	result
-		.then((data) => {
-			console.log(data);
-			res.json({ data: data });
-		})
-		.catch((err) => console.log(err));
+router.post("/orderByLocationAndInspectorAndEmpID", (req, res) => {
+  const empLatitude = req.body.empLatitude;
+  const empLongitude = req.body.empLongitude;
+  const empId = req.body.empId;
+  const result = db.orderByLocationAndInspectorAndEmpID(
+    empId,
+    empLatitude,
+    empLongitude
+  );
+  result
+    .then((data) => {
+      res.json({ data: data });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/orderByPriority", (req, res) => {
+  const result = db.orderByPriority();
+  result
+    .then((data) => {
+      console.log(data);
+      res.json({ data: data });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.post("/orderByPriorityAndEmpID", (req, res) => {
+  const empId = req.body.empId;
+  const result = db.orderByPriorityAndEmpID(empId);
+  result
+    .then((data) => {
+      console.log(data);
+      res.json({ data: data });
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
--- api/controller/testDbService.js
+++ api/controller/testDbService.js
@@ -1,190 +1,291 @@
-const mysql = require('mysql');
-const connection = require('./dbConnection');
-const haversine = require('haversine');
-const lodash = require('lodash');
-const { xor } = require('lodash');
+const mysql = require("mysql");
+const connection = require("./dbConnection");
+const haversine = require("haversine");
+const lodash = require("lodash");
+const { xor } = require("lodash");
 
 let instance = null;
 
 class Dbservice {
-	static getDbServiceInstance() {
-		return instance ? instance : new Dbservice();
-	}
+  static getDbServiceInstance() {
+    return instance ? instance : new Dbservice();
+  }
 
-	async getAllTests() {
-		try {
-			const response = await new Promise((resolve, reject) => {
-				const query = `SELECT * FROM test where DateCompleted is NULL OR DateCompleted = "0000-00-00 00:00:00"`;
-				connection.query(query, (err, results) => {
-					if (err) reject(new Error(err));
-					resolve(results);
-				});
-			});
-			return response;
-		} catch (error) {
-			console.log(error.message);
-		}
-	}
+  async getAllTests() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = `SELECT * FROM test where DateCompleted is NULL OR DateCompleted = "0000-00-00 00:00:00"`;
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-	async createNewTest(TestID, DateIssued, AssetID, InspectorID, SupervisorID, Frequency, Priority, TestModID) {
-		try {
-			const response = await new Promise((resolve, reject) => {
-				const query =
-					'INSERT INTO test (TestID,DateIssued, AssetID, InspectorID, SupervisorID, Frequency, Priority, TestModID, last_modified) Values ( ?, ?, ?, ?, ?, ?, ?, ?, UNIX_TIMESTAMP())';
+  async getAllTestsByEMPID(EmpID) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = `SELECT * FROM test where (DateCompleted is NULL OR DateCompleted = "0000-00-00 00:00:00") AND InspectorID = (?)`;
+        connection.query(query, [EmpID], (err, results) => {
+          if (err) reject(new Error(err));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-				connection.query(
-					query,
-					[ TestID, DateIssued, AssetID, InspectorID, SupervisorID, Frequency, Priority, TestModID ],
-					(err, results) => {
-						if (err) reject(err.message);
-						resolve('New test added');
-					}
-				);
-			});
-			return response;
-		} catch (error) {
-			console.log('There was an error');
-		}
-	}
+  async createNewTest(
+    TestID,
+    DateIssued,
+    AssetID,
+    InspectorID,
+    SupervisorID,
+    Frequency,
+    Priority,
+    TestModID
+  ) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query =
+          "INSERT INTO test (TestID,DateIssued, AssetID, InspectorID, SupervisorID, Frequency, Priority, TestModID, last_modified) Values ( ?, ?, ?, ?, ?, ?, ?, ?, UNIX_TIMESTAMP())";
 
-	async importTest(
-		TestID,
-		DateIssued,
-		AssetID,
-		InspectorID,
-		Result,
-		SupervisorID,
-		DateCompleted,
-		Frequency,
-		Priority,
-		TestModID,
-		comments
-	) {
-		try {
-			const response = await new Promise((resolve, reject) => {
-				const query =
-					'INSERT IGNORE INTO test (TestID,DateIssued, AssetID, InspectorID, Result, SupervisorID, DateCompleted, Frequency, Priority, TestModID, comments) Values ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-				connection.query(
-					query,
-					[
-						TestID,
-						DateIssued,
-						AssetID,
-						InspectorID,
-						Result,
-						SupervisorID,
-						DateCompleted,
-						Frequency,
-						Priority,
-						TestModID,
-						comments
-					],
-					(err, results) => {
-						if (err) reject(err.message);
-						resolve('New test imported');
-					}
-				);
-			});
-			return response;
-		} catch (error) {
-			console.log('There was an error');
-		}
-	}
+        connection.query(
+          query,
+          [
+            TestID,
+            DateIssued,
+            AssetID,
+            InspectorID,
+            SupervisorID,
+            Frequency,
+            Priority,
+            TestModID,
+          ],
+          (err, results) => {
+            if (err) reject(err.message);
+            resolve("New test added");
+          }
+        );
+      });
+      return response;
+    } catch (error) {
+      console.log("There was an error");
+    }
+  }
 
-	async setResult(TestID, Result, DateCompleted, comments) {
-		try {
-			const response = await new Promise((resolve, reject) => {
-				const query = 'UPDATE test SET Result = ?, DateCompleted = ?, comments = ? WHERE TestID = ?';
-				connection.query(query, [ Result, DateCompleted, comments, TestID ], (err, results) => {
-					if (err) reject(err.message);
-					resolve('Record updated');
-				});
-			});
-			return response;
-		} catch (error) {
-			console.log(error.message);
-		}
-	}
+  async importTest(
+    TestID,
+    DateIssued,
+    AssetID,
+    InspectorID,
+    Result,
+    SupervisorID,
+    DateCompleted,
+    Frequency,
+    Priority,
+    TestModID,
+    comments
+  ) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query =
+          "INSERT IGNORE INTO test (TestID,DateIssued, AssetID, InspectorID, Result, SupervisorID, DateCompleted, Frequency, Priority, TestModID, comments) Values ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        connection.query(
+          query,
+          [
+            TestID,
+            DateIssued,
+            AssetID,
+            InspectorID,
+            Result,
+            SupervisorID,
+            DateCompleted,
+            Frequency,
+            Priority,
+            TestModID,
+            comments,
+          ],
+          (err, results) => {
+            if (err) reject(err.message);
+            resolve("New test imported");
+          }
+        );
+      });
+      return response;
+    } catch (error) {
+      console.log("There was an error");
+    }
+  }
 
-	async AutoInsertIntoRepair(AssetID, CreatedDate) {
-		try {
-			const response = await new Promise((resolve, reject) => {
-				const query = 'INSERT INTO repair(AssetID, CreatedDate) VALUES (?, ?)';
-				connection.query(query, [ AssetID, CreatedDate ], (err, results) => {
-					console.log(query);
-					if (err) reject(err.message);
-					resolve('Repair Entry Added');
-				});
-			});
-			return response;
-		} catch (error) {
-			console.log(error.message);
-		}
-	}
+  async setResult(TestID, Result, DateCompleted, comments) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query =
+          "UPDATE test SET Result = ?, DateCompleted = ?, comments = ? WHERE TestID = ?";
+        connection.query(
+          query,
+          [Result, DateCompleted, comments, TestID],
+          (err, results) => {
+            if (err) reject(err.message);
+            resolve("Record updated");
+          }
+        );
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-	async orderByLocationAndInspector(empId, empLatitude, empLongitude) {
-		try {
-			const employeeCoordinates = {
-				latitude: empLatitude,
-				longitude: empLongitude
-			};
-			let nearByAssets = [];
-			const response = await new Promise((resolve, reject) => {
-				const query = `SELECT t.InspectorID, a.GPSLatitude, a.GPSLongitude, t.AssetID, t.TestID, t.TestModID
+  async AutoInsertIntoRepair(AssetID, CreatedDate) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "INSERT INTO repair(AssetID, CreatedDate) VALUES (?, ?)";
+        connection.query(query, [AssetID, CreatedDate], (err, results) => {
+          console.log(query);
+          if (err) reject(err.message);
+          resolve("Repair Entry Added");
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async orderByLocationAndInspector(empId, empLatitude, empLongitude) {
+    try {
+      const employeeCoordinates = {
+        latitude: empLatitude,
+        longitude: empLongitude,
+      };
+      let nearByAssets = [];
+      const response = await new Promise((resolve, reject) => {
+        const query = `SELECT t.InspectorID, a.GPSLatitude, a.GPSLongitude, t.AssetID, t.TestID, t.TestModID
                         from test t, asset a
                         where t.AssetID = a.AssetID
                         AND (DateCompleted is NULL OR  DateCompleted = "0000-00-00 00:00:00")`;
-				connection.query(query, [ empId ], (err, results) => {
-					if (err) reject(new Error(err));
-					console.log('Words');
-					console.log(results);
-					results.forEach((element) => {
-						let asset = {
-							latitude: element.GPSLatitude,
-							longitude: element.GPSLongitude
-						};
-						const distance = Math.round(
-							haversine(asset, employeeCoordinates, {
-								unit: 'meter'
-							})
-						);
-						if (distance) {
-							nearByAssets.push({
-								distance: distance,
-								AssetID: element.AssetID,
-								InspectorID: element.InspectorID,
-								TestID: element.TestID,
-								TestModID: element.TestModID
-							});
-						}
-					});
-					resolve(
-						lodash.sortBy(nearByAssets, (e) => {
-							return e.distance;
-						})
-					);
-				});
-			});
-			return response;
-		} catch (error) {
-			console.log(error.message);
-		}
-	}
+        connection.query(query, [empId], (err, results) => {
+          if (err) reject(new Error(err));
+          console.log(results);
+          results.forEach((element) => {
+            let asset = {
+              latitude: element.GPSLatitude,
+              longitude: element.GPSLongitude,
+            };
+            const distance = Math.round(
+              haversine(asset, employeeCoordinates, {
+                unit: "meter",
+              })
+            );
+            if (distance) {
+              nearByAssets.push({
+                distance: distance,
+                AssetID: element.AssetID,
+                InspectorID: element.InspectorID,
+                TestID: element.TestID,
+                TestModID: element.TestModID,
+              });
+            }
+          });
+          resolve(
+            lodash.sortBy(nearByAssets, (e) => {
+              return e.distance;
+            })
+          );
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-	async orderByPriority() {
-		try {
-			const response = await new Promise((resolve, reject) => {
-				const query = `SELECT * from test where DateCompleted is NULL or DateCompleted = "0000-00-00 00:00:00" ORDER by Priority ASC`;
-				connection.query(query, (err, results) => {
-					if (err) reject(new Error(err));
-					resolve(results);
-				});
-			});
-			return response;
-		} catch (error) {
-			console.log(error.message);
-		}
-	}
+  async orderByLocationAndInspectorAndEmpID(empId, empLatitude, empLongitude) {
+    try {
+      const employeeCoordinates = {
+        latitude: empLatitude,
+        longitude: empLongitude,
+      };
+      let nearByAssets = [];
+      const response = await new Promise((resolve, reject) => {
+        const query = `SELECT t.InspectorID, a.GPSLatitude, a.GPSLongitude, t.AssetID, t.TestID, t.TestModID
+                        from test t, asset a
+                        where t.AssetID = a.AssetID
+                        AND (DateCompleted is NULL OR  DateCompleted = "0000-00-00 00:00:00")
+						AND t.InspectorID = (?)`;
+        connection.query(query, [empId], (err, results) => {
+          if (err) reject(new Error(err));
+          console.log(results);
+          results.forEach((element) => {
+            let asset = {
+              latitude: element.GPSLatitude,
+              longitude: element.GPSLongitude,
+            };
+            const distance = Math.round(
+              haversine(asset, employeeCoordinates, {
+                unit: "meter",
+              })
+            );
+            if (distance) {
+              nearByAssets.push({
+                distance: distance,
+                AssetID: element.AssetID,
+                InspectorID: element.InspectorID,
+                TestID: element.TestID,
+                TestModID: element.TestModID,
+              });
+            }
+          });
+          resolve(
+            lodash.sortBy(nearByAssets, (e) => {
+              return e.distance;
+            })
+          );
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async orderByPriority() {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = `SELECT * from test where DateCompleted is NULL or DateCompleted = "0000-00-00 00:00:00" ORDER by Priority ASC`;
+        connection.query(query, (err, results) => {
+          if (err) reject(new Error(err));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async orderByPriorityAndEmpID(empId) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = `SELECT * from test where (DateCompleted is NULL or DateCompleted = "0000-00-00 00:00:00") AND InspectorID = (?) ORDER by Priority ASC`;
+        connection.query(query, [empId], (err, results) => {
+          if (err) reject(new Error(err));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 }
 
 module.exports = Dbservice;
